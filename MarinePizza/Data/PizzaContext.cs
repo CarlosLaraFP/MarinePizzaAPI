9a06043b6fc456b1ffb816bbff08638cfe46b5cd
CarlosLaraFP <info@carloslaraai.com>
2023-08-21 08:02:23 -0700

    Renamed project

---
+++ MarinePizza/Data/PizzaContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using MarinePizza.Models;
+
+namespace MarinePizza.Data;
+
+// DbContext is a gateway through which you can interact with the database.
+// When instantiated, PizzaContext will expose Pizzas, Toppings, and Sauces properties.
+// Changes you make to the collections that are exposed by those properties will be propagated to the database.
+public class PizzaContext : DbContext
+{
+    // The constructor allows external code to pass in the configuration so that the same DbContext
+    // can be shared between test and production code, and even be used with different providers.
+    public PizzaContext(DbContextOptions<PizzaContext> options) : base(options)
+    {
+    }
+
+    // DbSet<T> properties correspond to tables to create in the database.
+    public DbSet<Pizza> Pizzas => Set<Pizza>();
+    public DbSet<Topping> Toppings => Set<Topping>();
+    public DbSet<Sauce> Sauces => Set<Sauce>();
+}
---
+++ MarinePizza/Program.cs
@@ -0,0 +1,41 @@
+﻿using MarinePizza.Services;
+using MarinePizza.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add PizzaContext
+builder.Services.AddSqlite<PizzaContext>("Data Source=MarinePizza.db");
+
+// Add PromotionsContext
+
+// PizzaService is registered with the ASP.NET Core dependency injection system
+builder.Services.AddScoped<PizzaService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Add the CreateDbIfNotExists method call
+
+app.MapGet("/", () => @"Pizza management API. Navigate to /swagger to open the Swagger test UI.");
+
+app.Run();
+
